cb6713177f4ec66b9b8666dfd6e8f3f48adc273b
Hafra121 <hafrailliyas@gmail.com>
2024-12-24 22:18:42 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
 url = https://docs.google.com/document/d/15jZQXp9Hn7RD_9r0BtH_B7j1EkH3PQZh/edit
+
+python input: print('hello,world!')
+
+python code output: ![Screenshot_20241224_221325.jpg](https://github.com/user-attachments/assets/76f63ed2-3c6b-4fc1-a524-45b3d5417ee4
